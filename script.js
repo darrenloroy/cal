d0b95e319142663852656a1a3d7b24a2d15d1f9b
darrenloroy <darrenloroy@gmail.com>
2024-07-16 13:52:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -52,8 +52,43 @@ const performCalculation = {
     '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
     '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '^': (firstOperand, secondOperand) => Math.pow(firstOperand, secondOperand),
     '=': (firstOperand, secondOperand) => secondOperand
 };
+
+function handleFunction(func) {
+    const { displayValue } = calculator;
+    const inputValue = parseFloat(displayValue);
+    let result;
+
+    switch (func) {
+        case 'sin':
+            result = Math.sin(inputValue);
+            break;
+        case 'cos':
+            result = Math.cos(inputValue);
+            break;
+        case 'tan':
+            result = Math.tan(inputValue);
+            break;
+        case 'log':
+            result = Math.log10(inputValue);
+            break;
+        case 'ln':
+            result = Math.log(inputValue);
+            break;
+        case 'sqrt':
+            result = Math.sqrt(inputValue);
+            break;
+        case 'pi':
+            result = Math.PI;
+            break;
+        default:
+            return;
+    }
+
+    calculator.displayValue = String(result);
+}
 
 function resetCalculator() {
     calculator.displayValue = '0';
@@ -78,6 +113,12 @@ keys.addEventListener('click', (event) => {
 
     if (target.classList.contains('operator')) {
         handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('function')) {
+        handleFunction(target.value);
         updateDisplay();
         return;
     }
